79d639b209d3ad1837739ee373bca1cd43b0a728
RHENAN COCEV LUIS <rhenan.luis@viavarejo.com.br>
2022-08-29 05:28:34 -0300

    new version cronjob

--- agendamento-bate-ponto.js
+++ agendamento-bate-ponto.js
@@ -1,15 +1,9 @@
 require('dotenv').config();
-const CronJob = require('cron').CronJob
+const schedule = require('node-schedule');
 const bate_ponto = require('./bate-ponto')
 const random = require('./random')
 
-async function cronActive (ctx,bot,hardStop){
-
-    if(hardStop){
-        console.log('CRONJOB DESATIVADO')
-        await bot.sendMessage(ctx, 'Modo hard stop selecionado.')
-        process.exit(1)
-    }
+async function cronActive (ctx,bot,status){
 
     const timeElapsed = Date.now();
     const today = new Date(timeElapsed);
@@ -32,44 +26,61 @@ async function cronActive (ctx,bot,hardStop){
         minuto_saida = (cron_entrada + cron_saida) % 60
     }
 
-    console.log('===================== ' + today.toLocaleDateString() + ' =====================')
-    console.log('Sua entrada vai ser 9:' + cron_entrada);
-    console.log('Sua entrada do almoço vai ser 12:'+ cron_entrada_almoco);
-    console.log('Sua saida do almoço vai ser 13:'+ minuto_saida_almoco);
-    console.log('Sua saida vai ser '+ hora_saida + ':' + minuto_saida);
-    console.log('===================== AGUARDANDO SCHEDULE =====================')
+    const scheduleList = schedule.scheduledJobs;
+    if (scheduleList['entrada', 'almoco', 'volta_almoco', 'saida'] != undefined) {
+        //Nesse caso, caso ele ache o nome do processo na lista vamos utilizar o nome dele para cancela-lo.
+        schedule.gracefulShutdown();
+    }
 
-    bot.sendMessage(chatId, 'DATA: ' + today.toLocaleDateString() + ' \n'
-                    + '\nSua entrada vai ser 9:' + cron_entrada
-                    + '\nSua entrada do almoço vai ser 12:'+ cron_entrada_almoco
-                    + '\nSua saida do almoço vai ser 13:'+ minuto_saida_almoco
-                    + '\nSua saida vai ser '+ hora_saida + ':' + minuto_saida
-                    + '\n\nSTATUS: AGUARDANDO SCHEDULE');
+    if (status){
+        console.log('===================== ' + today.toLocaleDateString() + ' =====================')
+        console.log('Sua entrada vai ser 9:' + cron_entrada);
+        console.log('Sua entrada do almoço vai ser 12:'+ cron_entrada_almoco);
+        console.log('Sua saida do almoço vai ser 13:'+ minuto_saida_almoco);
+        console.log('Sua saida vai ser '+ hora_saida + ':' + minuto_saida);
+        console.log('===================== AGUARDANDO SCHEDULE =====================')
+    
+        bot.sendMessage(chatId, 'DATA: ' + today.toLocaleDateString() + ' \n'
+                        + '\nSua entrada vai ser 9:' + cron_entrada
+                        + '\nSua entrada do almoço vai ser 12:'+ cron_entrada_almoco
+                        + '\nSua saida do almoço vai ser 13:'+ minuto_saida_almoco
+                        + '\nSua saida vai ser '+ hora_saida + ':' + minuto_saida
+                        + '\n\nSTATUS: AGUARDANDO SCHEDULE');
+    }
+    
+        const entrada = schedule.scheduleJob('entrada', cron_entrada + ' 9 * * 1-5', () => {
+            console.log('Iniciando cronJOB para bater o ponto de entrada as 09:' + cron_entrada)
+            bot.sendMessage(chatId, 'Iniciando cronJOB para bater o ponto de entrada as 09:' + cron_entrada)
+            bate_ponto.aponta(chatId,bot);
+        }, null, true, 'America/Sao_Paulo')
+    
+        const almoco = schedule.scheduleJob('almoco', cron_entrada_almoco + ' 12 * * 1-5', () => {
+            console.log('Iniciando cronJOB para bater o ponto de entrada do almoço as 12:' + cron_entrada_almoco)
+            bot.sendMessage(chatId, 'Iniciando cronJOB para bater o ponto de entrada do almoço as 12:' + cron_entrada_almoco)
+            bate_ponto.aponta(chatId,bot);
+        }, null, true, 'America/Sao_Paulo')
+    
+        const volta_almoco = schedule.scheduleJob('volta_almoco', minuto_saida_almoco + ' 13 * * 1-5', () => {
+            console.log('Iniciando cronJOB para bater o ponto de saida do almoço as 13:' + minuto_saida_almoco)
+            bot.sendMessage(chatId, 'Iniciando cronJOB para bater o ponto de saida do almoço as 13:' + minuto_saida_almoco)
+            bate_ponto.aponta(chatId,bot);
+        }, null, true, 'America/Sao_Paulo')
+    
+        const saida = schedule.scheduleJob('saida', minuto_saida + ' ' + hora_saida + ' * * 1-5', async () => {
+            console.log('Iniciando cronJOB para bater o ponto de saida as ' + hora_saida + ':' + minuto_saida)
+            bot.sendMessage(chatId, 'Iniciando cronJOB para bater o ponto de saida as ' + hora_saida + ':' + minuto_saida)
+            await bate_ponto.aponta(chatId,bot);
+            process.exit(1)
+        }, null, true, 'America/Sao_Paulo')
 
-    const entrada = new CronJob(cron_entrada + ' 9 * * 1-5', () => {
-        console.log('Iniciando cronJOB para bater o ponto de entrada as 09:' + cron_entrada)
-        bot.sendMessage(chatId, 'Iniciando cronJOB para bater o ponto de entrada as 09:' + cron_entrada)
-        bate_ponto.aponta(chatId,bot);
-    }, null, true, 'America/Sao_Paulo')
-
-    const almoco = new CronJob(cron_entrada_almoco + ' 12 * * 1-5', () => {
-        console.log('Iniciando cronJOB para bater o ponto de entrada do almoço as 12:' + cron_entrada_almoco)
-        bot.sendMessage(chatId, 'Iniciando cronJOB para bater o ponto de entrada do almoço as 12:' + cron_entrada_almoco)
-        bate_ponto.aponta(chatId,bot);
-    }, null, true, 'America/Sao_Paulo')
-
-    const volta_almoco = new CronJob(minuto_saida_almoco + ' 13 * * 1-5', () => {
-        console.log('Iniciando cronJOB para bater o ponto de saida do almoço as 13:' + minuto_saida_almoco)
-        bot.sendMessage(chatId, 'Iniciando cronJOB para bater o ponto de saida do almoço as 13:' + minuto_saida_almoco)
-        bate_ponto.aponta(chatId,bot);
-    }, null, true, 'America/Sao_Paulo')
-
-    const saida = new CronJob(minuto_saida + ' ' + hora_saida + ' * * 1-5', async () => {
-        console.log('Iniciando cronJOB para bater o ponto de saida as ' + hora_saida + ':' + minuto_saida)
-        bot.sendMessage(chatId, 'Iniciando cronJOB para bater o ponto de saida as ' + hora_saida + ':' + minuto_saida)
-        await bate_ponto.aponta(chatId,bot);
-        process.exit(1)
-    }, null, true, 'America/Sao_Paulo')
+        if(status == false){
+            entrada.cancel()
+            almoco.cancel()
+            volta_almoco.cancel()
+            saida.cancel()
+            console.log('cancelados')
+            schedule.gracefulShutdown();
+        }
 
 }
 
--- app-telegram.js
+++ app-telegram.js
@@ -2,16 +2,21 @@ const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 const bate_ponto = require('./bate-ponto')
 
+const teste = require('./teste')
+
 const agendamento = require('./agendamento-bate-ponto')
 
 const token = process.env.TOKEN;
-const chat_id = process.env.CHAT_ID;
+const chat_id = 621550962;
 // replace the value below with the Telegram token you receive from @BotFather
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
 process.env["NTBA_FIX_350"] = 1;
 process.env.NTBA_FIX_319 = 1;
+
+//precisei inicializar aqui, por conta do exit process.
+agendamento.cronActive(chat_id,bot,true);
 
 bot.onText(/\/aponta/, (ctx,match) => {
     const chatId = ctx.chat.id;
@@ -27,10 +32,10 @@ bot.onText(/\/aponta/, (ctx,match) => {
 bot.onText(/\/start/, async (ctx,match) => {
     const chatId = ctx.chat.id;
     const nome = ctx.from.first_name;
-    var hardStop = false;
+    var ativar = true;
     if (chatId === chat_id){
         await bot.sendMessage(chatId, nome + ", ja schedulei seu job:");
-        agendamento.cronActive(chatId,bot,hardStop);
+        agendamento.cronActive(chatId,bot,ativar);
     } else {
         bot.sendMessage(chatId, nome + ", você não está autorizado para utilizar o bot.");
     } 
@@ -40,10 +45,10 @@ bot.onText(/\/start/, async (ctx,match) => {
 bot.onText(/\/stop/, async (ctx,match) => {
     const chatId = ctx.chat.id;
     const nome = ctx.from.first_name;
-    var hardStop = true;
+    var ativar = false;
     if (chatId === chat_id){
         await bot.sendMessage(chatId, nome + ", cancelei todos os seus schedules");
-        agendamento.cronActive(chatId,bot,hardStop);
+        agendamento.cronActive(chatId,bot,ativar);
     } else {
         bot.sendMessage(chatId, nome + ", você não está autorizado para utilizar o bot.");
     } 
