392a0f6b890da43337320521eca8fa2377ebe903
rhenancocev <rhenan_cocev01@hotmail.com>
2026-02-16 06:37:56 -0300

    Version for aks

--- src/tokenAcesso/serverTelegramBot.js
+++ src/tokenAcesso/serverTelegramBot.js
@@ -1,8 +1,14 @@
 const env = require('../../config');
 const TelegramBot = require('node-telegram-bot-api');
-// replace the value below with the Telegram token you receive from @BotFather
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(env.TOKEN, { polling: true });
+const bot = new TelegramBot(env.TOKEN, {
+  polling: {
+    autoStart: true,
+    interval: 300,
+    params: {
+      timeout: 10
+    }
+  }
+});
 
 module.exports = bot;
