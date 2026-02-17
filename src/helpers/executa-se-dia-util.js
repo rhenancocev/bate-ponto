555c2e2b08c87032e16c43bc27506a68728c70ae
rhenancocev <rhenan_cocev01@hotmail.com>
2026-02-17 03:44:04 -0300

    Version for aks

---
+++ src/helpers/executa-se-dia-util.js
@@ -0,0 +1,16 @@
+const feriados = require('./feriados');
+
+module.exports = async function executaSeDiaUtil(bot, chatId, fn) {
+  if (feriados.isFeriado()) {
+    const nome = feriados.nomeFeriado();
+
+    await bot.sendMessage(
+      chatId,
+      `Hoje é feriado (${nome}). Nenhum ponto será executado.`
+    );
+
+    return;
+  }
+
+  await fn();
+};
